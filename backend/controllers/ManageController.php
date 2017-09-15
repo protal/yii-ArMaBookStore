b16b59bb976b78d0c3c7067d9be91f58ba3b3f5f
Harit <slipknot.opal@gmail.com>
2017-09-15 23:57:14 +0700

    add function book save for View NEW and EDIT

--- backend/controllers/ManageController.php
+++ backend/controllers/ManageController.php
@@ -6,6 +6,7 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use common\models\LoginForm;
+use backend\models\Book;
 
 /**
  * Site controller
@@ -43,5 +44,47 @@ class ManageController extends Controller
     {
         $this->layout = "@backend/themes/adminlte/layouts/index";
         return $this->render('index');
+    }
+
+    /**
+     * Add book
+     *
+     * @return string
+     */
+    public function actionBooksave()
+    {
+        //config
+        $request = Yii::$app->request;
+        $baseUrl = \Yii::getAlias('@web');
+
+        $id = $request->get('id',null);
+        $name = $request->get('name',null);
+        $type = $request->get('type',null);
+        $price = $request->get('price',null);
+        $days = $request->get('days',null);
+        $charge = $request->get('charge',null);
+        $total = $request->get('total',null);
+        if($id == null){
+          $book = new Book;
+        }else {
+          $book = Book::findOne($id);
+        }
+
+        $book->name = $name;
+        $book->type = $type;
+        $book->price = $price;
+        $book->days = $days;
+        $book->charge = $charge;
+        $book->total = $total;
+
+        if($book->save()){
+          echo "success";
+        }else {
+          echo "error";
+        }
+
+        
+        //waiting redirect
+        // return $this->redirect($baseUrl."/course/index");
     }
 }
