4c16cd9050328cd29286e6d5ccad1bf7c30b7bed
Harit <slipknot.opal@gmail.com>
2017-09-15 23:56:14 +0700

    add model BOOK :cat:

---
+++ backend/models/Book.php
@@ -0,0 +1,56 @@
+<?php
+namespace backend\models;
+
+use Yii;
+
+
+/**
+ * This is the model class for collection "lecturer".
+ *
+ * @property \MongoDB\BSON\ObjectID|string $_id
+ * @property mixed $name
+ * @property String $dob
+ * @property mixed $gender
+ * @property mixed $degree
+ * @property mixed $address
+ * @property mixed $email
+ * @property mixed $phone_number
+ */
+class Book extends \yii\mongodb\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function collectionName()
+    {
+        return ['BookStore', 'book'];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributes()
+    {
+        return [
+            '_id',
+            'name',
+            'type',
+            'price',
+            'charge',
+            'days',
+            'total'
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['_id', 'name', 'type', 'price', 'charge','days', 'total'], 'safe']
+        ];
+    }
+}
+
+ ?>
