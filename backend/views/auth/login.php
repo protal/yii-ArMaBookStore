1927500c0db7c61d3b3b8ba174b2cfc450e72323
M.mahdee <mahdee.dark@gmail.com>
2017-09-18 02:17:00 +0700

    แก้ login นิดหน่อย

--- backend/views/auth/login.php
+++ backend/views/auth/login.php
@@ -118,7 +118,7 @@
     }
 
     body {
-      background: #76b852 !important;
+      background: #76b852 ;
       /* fallback for old browsers */
       background: -webkit-linear-gradient(right, #76b852, #8DC26F);
       background: -moz-linear-gradient(right, #76b852, #8DC26F);
