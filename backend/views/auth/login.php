dc30c574c35cb9685a76989501d0bf6f17fbc60f
Harit <slipknot.opal@gmail.com>
2017-09-19 13:12:51 +0700

    butify form login

--- backend/views/auth/login.php
+++ backend/views/auth/login.php
@@ -4,29 +4,23 @@ $baseUrl=\Yii::getAlias('@web');
  ?>
 
  <div class="container h-100 d-flex justify-content-center">
-   <div class="row">
-     <div class="my-auto">
-       <form class="form-horizontal" role="form" method="post" action="<?=$baseUrl."/auth/loginaction"?>">
+       <form role="form" method="post" action="<?=$baseUrl."/auth/loginaction"?>">
              <h2>Login</h2>
              <hr>
              <div class="form-group">
                <label class="sr-only" for="email">E-Mail</label>
                <div class="input-group">
-                 <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-at"></i></div>
                  <input type="text" name="email" class="form-control" id="email" placeholder="you@example.com" required autofocus>
                </div>
              </div>
              <div class="form-group">
                <label class="sr-only" for="password">Password</label>
                <div class="input-group">
-                 <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-key"></i></div>
                  <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
                </div>
              </div>
 
              <button type="submit" class="btn btn-success"><i class="fa fa-sign-in"></i> Login</button>
-             <a class="btn btn-link" href="/auth/register"> Register </a>
+             <a class="btn btn-link" href="<?=$baseUrl."/auth/register"?>"> Register </a>
        </form>
-     </div>
-   </div>
  </div>
