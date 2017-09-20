21db5d04fc4a8549b4c4d684c1debb006d26d8d6
prapaporn15072540 <@DESKTOP-4AI1B3D>
2017-09-20 16:28:27 +0700

    bookhistory *ยังไม่มี db

--- backend/views/manage/bookhistory.php
+++ backend/views/manage/bookhistory.php
@@ -2,73 +2,32 @@
 $this->title = 'BookStore Log File';
 $baseUrl=\Yii::getAlias('@web');
 use yii\web\View;
-$str = <<<EOT
-$('#fresh-table').bootstrapTable({
-    toolbar: ".toolbar",
+?>
+<table border="1" class="table table-striped">
 
-    showRefresh: true,
-    search: true,
-    showToggle: true,
-    showColumns: true,
-    pagination: true,
-    striped: true,
-    pageSize: 8,
-    pageList: [8,10,25,50,100],
+	<tr>
+		<th  data-field="name" data-sortable="true">ชื่อผู้ยืม</th>
+		<th  data-field="" data-sortable="true">รายการหนังสือ</th>
+		<th  data-field="" data-sortable="true">สถานะ</th>
+    </tr>
 
-    formatShowingRows: function(pageFrom, pageTo, totalRows){
-        //do nothing here, we don't want to show the text "showing x of y from..."
-    },
-    formatRecordsPerPage: function(pageNumber){
-        return pageNumber + " rows visible";
-    },
-    icons: {
-        refresh: 'fa fa-refresh',
-        toggle: 'fa fa-th-list',
-        columns: 'fa fa-columns',
-        detailOpen: 'fa fa-plus-circle',
-        detailClose: 'fa fa-minus-circle'
-    }
-});
-$(window).resize(function () {
-    $('#fresh-table').bootstrapTable('resetView');
-});
-EOT;
-$this->registerJS($str,View::POS_LOAD,'form-js');
+	
+      </td>
+      <td>1</td>
+  		<td>1</td>
+  		<td>
+  			<select>
+  				<option value="กำลังจัดส่ง">กำลังจัดส่ง</option>
+  				<option value="จัดส่งแล้ว">จัดส่งแล้ว</option>
+  				<option value="ใกล้ถึงกำหนดคืน">ใกล้ถึงกำหนดคืน</option>
+  				<option value="คืนช้าเสียค่าปรับ">คืนช้าเสียค่าปรับ</option>
+				<option value="ยกเลิกการจัดส่ง">ยกเลิกการจัดส่ง</option>
+			</select> 
+		</td>
+  		
+  	</tr>
+   
 
- ?>
+</table>
 
-<div class="fresh-table full-color-azure">
-  <!--    Available colors for the full background: full-color-blue, full-color-azure, full-color-green, full-color-red, full-color-orange
-  Available colors only for the toolbar: toolbar-color-blue, toolbar-color-azure, toolbar-color-green, toolbar-color-red, toolbar-color-orange
-  -->
 
-  <div class="toolbar">
-    <button class="btn btn-default">กลับ</button>
-    <button class="btn btn-default">รายการหนังสือ</button>
-  </div>
-
-  <table id="fresh-table" class="table">
-    <thead>
-       
-       
-      <th data-field="name" data-sortable="true">ชื่อผู้ยืม</th>
-      <th data-field="" data-sortable="true">รายการหนังสือ</th>
-      <th data-field="" data-sortable="true">สถานะ</th>
-      
-       <!-- <th data-field="actions" data-formatter="operateFormatter" data-events="operateEvents">Actions</th> -->
-    </thead>
-    <tbody>
-       
-      <?php foreach ($result as $var){?>
-	       <tr>
-              
-        		<td>1</td>
-        		<td>2</td>
-        		<td>3</td>
-        		
-  	   </tr>
-   <?php }?>
-
-    </tbody>
-  </table>
-</div>
