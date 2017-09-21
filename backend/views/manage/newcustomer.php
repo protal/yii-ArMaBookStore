f4372b609b94d6d44e0ec69f5b01cf69a7b1e02c
DESKTOP-5D1H62T\User <chid2539@icloud.com>
2017-09-22 06:39:24 +0700

    คนสวย

--- backend/views/manage/newcustomer.php
+++ backend/views/manage/newcustomer.php
@@ -2,48 +2,47 @@
 $this->title = 'ArmaBook Add New Customer';
 $baseUrl=\Yii::getAlias('@web');
 ?>
-
+<div class="container h-100 d-flex justify-content-center">
 <form action="<?=$baseUrl."/manage/customersave"?>">
-  	  
-  	
-   
-   	<div class="form-group row">
-  		<div class="col-xs-3">
+
+
+  <div class="card text-white bg-info " style="width: 30rem;">
+    <div class="card-header"> เพิ่มสมาชิก </div>
+    <div class="card-body">
+  
+  		<div class="col-md-8">
     		<label for="ex1">ชื่อ</label>
     		<input type="text" class="form-control" name="firstname">
   		</div>
-  		<div class="col-xs-3">
+
+  		<div class="col-md-8">
     		<label for="ex2">นามสกุล</label>
     		<input type="text" class="form-control" name="lastname">
   		</div>
-  </div>
 
- 	<div class="form-group row" >
-		<div class="col-xs-3">
+		<div class="col-md-8">
     		<label>เบอโทรศัพท์</label>
     		<input type="text" class="form-control" name="phone">
   		</div>
-  </div>
-   
-    <div class="form-group row" >
-		<div class="col-xs-3">
+
+		<div class="col-md-8">
     		<label>อีเมลล์</label>
     		<input type="text" class="form-control" name="email" >
   		</div>
-  </div>
-  <div class="form-group row" >
-		<div class="col-xs-3">
+
+		<div class="col-md-8">
     		<label>รหัสผ่าน</label>
     		<input type="text" class="form-control" name="password" >
   		</div>
-  </div>
-    <div class="form-group row" >
-		<div class="col-xs-3">
+
+		<div class="col-md-8">
     		<label>ที่อยู่</label>
     		<input type="text" class="form-control" name="address" >
   		</div>
-  </div>
-   
-   
-<button type="submit" class="btn btn-primary">ยืนยัน</button>
+<br>
+
+<center><button type="submit"  class="btn btn-danger">ยืนยัน</button></center>
 </form>
+</div>
+</div>
+</div>
